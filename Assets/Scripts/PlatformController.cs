1ce8f328dde4b0d217047a14ed26593227a120a6
Aidan Robinson <aidanrobinson6@gmail.com>
2019-10-21 11:47:45 +1300

    Basic Character

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController
+{
+    public LayerMask passengerMask;
+    public Vector3 move;
+
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+
+    void Update()
+    {
+        Vector3 velocity = move * Time.deltaTime;
+        transform.Translate(velocity);
+    }
+
+    void MovePassengers(Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        //vertically moving platform
+        if (velocity.y != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+                rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+                if(hit)
+                {
+
+                }
+            }
+        }
+    }
+}
