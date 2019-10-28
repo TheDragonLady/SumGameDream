bc3e78048fa3c08b85bfc247224643763cebfef1
Aidan Robinson <aidanrobinson6@gmail.com>
2019-10-29 11:52:36 +1300

    Fixing Moveable Blocks

---
+++ Assets/Scripts/PlayerPush.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPush : MonoBehaviour
+{
+    public float distance = 1f;
+    public LayerMask boxMask;
+    public float width;
+    public Vector2 boxDetector;
+
+    GameObject box;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        boxDetector = transform.position;
+        boxDetector.x += width;
+
+        RaycastHit2D hit = Physics2D.Raycast(boxDetector, Vector2.right, distance, boxMask);
+        Debug.Log(hit.collider);
+
+        if(hit.collider !=null && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("button is pressed");
+            box = hit.collider.gameObject;
+
+            box.GetComponent<FixedJoint2D>().enabled = true;
+            box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        Gizmos.DrawLine(boxDetector, (Vector2)transform.position + Vector2.right * distance);
+    }
+}
